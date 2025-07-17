a1819a6d878d12ce168a667794a511c7eff0c215
Miguel luis <126804912+Miguelluisdev@users.noreply.github.com>
2025-07-17 19:55:54 -0300

    Add GUI tests for project and issue creation
    Introduces new Cypress tests for creating projects and issues via the GUI. Enhances login command to support session caching and adds custom commands for project and issue creation. Minor updates to existing test files for consistency and documentation.

--- cypress/e2e/gui/login.cy.js
+++ cypress/e2e/gui/login.cy.js
@@ -1,7 +1,11 @@
-describe('Login', () => {
-  it('successfully', () => {
-    cy.login()
+describe("Login", () => {
+  it("successfully", () => {
+    const user = Cypress.env("user_name");
+    const password = Cypress.env("user_password");
+    const options = { cacheSession: false };
 
-    cy.get('.qa-user-avatar').should('be.visible')
-  })
-})
+    cy.login(user, password, options);
+
+    cy.get(".qa-user-avatar").should("be.visible");
+  });
+});
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,4 +2,7 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
+
+  // https://serverest.dev/
+  // para testar a  api
 })
